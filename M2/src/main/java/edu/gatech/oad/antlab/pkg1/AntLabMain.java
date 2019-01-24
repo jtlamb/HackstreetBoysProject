937f7693984e8c1d92597e48714e684aecd8db75
Anisha Bhogale <anisha.bhogale@gmail.com>
2019-01-24 17:20:31 -0500

    Changed gtid on person 3

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Anisha");
-		  toPrint += p3.toString("903393024");
+		  toPrint += p3.toString("abhogale6");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Caroline");
